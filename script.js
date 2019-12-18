a86d157678afc6742c55cfce85223a31104c8603
sin-alpha-pi <53899103+sin-alpha-pi@users.noreply.github.com>
2019-12-18 06:45:17 +0100

    Fusion und Bilder

--- script.js
+++ script.js
@@ -8,71 +8,61 @@ if (isChrome) {
 }
 
 function onlyOnChrome() {
-const welcomeText = document.querySelector('.welcome-text');
-const logo = document.querySelector('.logo');
-const hamburger = document.querySelector('.hamburger');
-const arrow = document.querySelector('.down-arrow');
-const navLinks = document.querySelector('.nav-links');
-const stylesheet = document.querySelector('.stylesheet');
-const scripts = document.querySelector('.scripts');
-const cirpLogo = document.querySelector('.cirp-logo');
+    const welcomeText = document.querySelector('.welcome-text');
+    const logo = document.querySelector('.logo');
+    const hamburger = document.querySelector('.hamburger');
+    const arrow = document.querySelector('.down-arrow');
+    const navLinks = document.querySelector('.nav-links');
+    const stylesheet = document.querySelector('.stylesheet');
+    const scripts = document.querySelector('.scripts');
 
+    let counter = 0;
+    let forewards = true;
 
-let counter = 0;
-let forewards = true;
+    hamburger.addEventListener('click', () => {
+        counter++;
+        navLinks.classList.toggle('open');
+        hamburger.classList.toggle('open');
+        if ((counter % 2) == 1) {
+            welcomeText.classList.add('vanish');
+            welcomeText.classList.remove('appear');
+            arrow.classList.add('vanish');
+            arrow.classList.remove('appear');
+        } else {
+            welcomeText.classList.add('appear');
+            welcomeText.classList.remove('vanish');
+            arrow.classList.add('appear');
+            arrow.classList.remove('vanish');
+        }
+    });
 
-hamburger.addEventListener('click', () => {
-    counter++;
-    navLinks.classList.toggle('open');
-    hamburger.classList.toggle('open');
-    if ((counter % 2) == 1) {
-        cirpLogo.classList.add('vanish');
-        cirpLogo.classList.remove('appear');
+    //scrollMagic
 
-        welcomeText.classList.add('vanish');
-        welcomeText.classList.remove('appear');
+    const tl = new TimelineLite();
+    tl.to('.nav-bar', 0.5, {
+            opacity: 1,
+            background: "white",
+            borderBottom: "3px solid #d1a33f",
+            paddingBottom: '7%'
+        })
+        .to('.introduction', 0.25, {
+            opacity: 0
+        }, '-=0.5')
+        .to('.hamburger', 0.1, {
+            zIndex: 4
+        })
+        .to('.down-arrow', 0.25, {
+            className: '+=vanish'
+        }, '-=0.75')
+        .to('.sponsoring-logos', 0.25, {
+            opacity: 0
+        }, '-=0.5');
+    var controller = new ScrollMagic.Controller();
 
-        arrow.classList.add('vanish');
-        arrow.classList.remove('appear');
-    } else {
-        cirpLogo.classList.add('appear');
-        cirpLogo.classList.remove('vanish');
-
-        welcomeText.classList.add('appear');
-        welcomeText.classList.remove('vanish');
-
-        arrow.classList.add('appear');
-        arrow.classList.remove('vanish');
-    }
-});
-
-//scrollMagic
-
-const tl = new TimelineLite();
-tl.to('.nav-bar', 0.5, {
-        opacity: 1,
-        background: "white",
-        borderBottom: "3px solid #d1a33f",
-        paddingBottom: '7%'
-    })
-    .to('.introduction', 0.25, {
-        opacity: 0
-    }, '-=0.5')
-    .to('.hamburger', 0.1, {
-        zIndex: 4
-    })
-    .to('.down-arrow', 0.25, {
-        className: '+=vanish'
-    }, '-=0.75')
-    .to('.cirp-logo', 0.25, {
-        className: '+=vanish'
-    },'-=0.5');
-var controller = new ScrollMagic.Controller();
-
-var scene = new ScrollMagic.Scene({
-        triggerElement: '.welcome-text'
-    })
-    .addTo(controller)
-    .setTween(tl)
-    .triggerHook(0);
+    var scene = new ScrollMagic.Scene({
+            triggerElement: '.welcome-text'
+        })
+        .addTo(controller)
+        .setTween(tl)
+        .triggerHook(0);
 }
